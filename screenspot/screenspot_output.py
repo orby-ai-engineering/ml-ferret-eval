6f715f62625eefa51e93ec86cc32e574475a2232
Cheng Chang <cheng@orby.ai>
2024-08-01 14:23:26 -0700

    Update screenspot_output.py

--- screenspot/screenspot_output.py
+++ screenspot/screenspot_output.py
@@ -1,7 +1,7 @@
 """
 Usage:
---data_path: path of refcoco annotation. 
---image_path:  path of refcoco images. 
+--data_path: path of refcoco annotation.
+--image_path:  path of refcoco images.
 --answers-file: path of output result.
 
 Example:
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python -m ferret.eval.model_refcoco \
     --answers-file refexp_result/finetune_refcocog_test \
     --add_region_feature \
     --chunk-idx 0 \
-    --num-chunks 1 
+    --num-chunks 1
 
 """
 
@@ -89,7 +89,7 @@ def find_bbox_template(text, img_w, img_h):
         new_box = resize_bbox([x1, y1, x2, y2], img_w, img_h)
         new_bboxes.append(new_box)
         old_bboxes.append([x1, y1, x2, y2])
-    
+
     set_old_bboxes = sorted(set(map(tuple, old_bboxes)), key=list(map(tuple, old_bboxes)).index)
     list_old_bboxes = list(map(list, set_old_bboxes))
 
@@ -99,7 +99,7 @@ def find_bbox_template(text, img_w, img_h):
     for i in range(len(list_bboxes)):
         x1, y1, x2, y2 = list_old_bboxes[i]
         text = text.replace('[{}, {}, {}, {}]'.format(x1, y1, x2, y2), '')
-    
+
     if text.endswith(" ."):
         text = text[:-2]
     split_text = text.split(" . ")
@@ -113,7 +113,7 @@ class ScreenSpotGrounding(torchvision.datasets.CocoDetection):
         with open(ann_file, "r") as f:
             self.targets = [json.loads(line) for line in f]
         self.question_prompt = "In this UI screenshot, what is the position of the element corresponding to the command <obj> (with point)?"
-    
+
     def __getitem__(self, idx):
         img, _ = super(ScreenSpotGrounding, self).__getitem__(idx)
         target = self.targets[idx]
@@ -146,7 +146,7 @@ def eval_model_screenspot(args):
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
     tokenizer, model, image_processor, _ = load_pretrained_model(model_path, args.model_base, model_name)
-    
+
     chunk_data_ids = get_chunk(data_ids, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
     ans_file = open(answers_file, "w")
@@ -168,8 +168,11 @@ def eval_model_screenspot(args):
         conv.append_message(conv.roles[0], qs)
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
+
+        print(prompt)
+
         input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
-        image_tensor = image_processor.preprocess(img, return_tensors='pt', do_resize=True, 
+        image_tensor = image_processor.preprocess(img, return_tensors='pt', do_resize=True,
                                                   do_center_crop=False, size=[args.image_h, args.image_w])['pixel_values'][0]
         stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
         keywords = [stop_str]
@@ -186,8 +189,8 @@ def eval_model_screenspot(args):
                 max_new_tokens=1024,
                 use_cache=True,
                 stopping_criteria=[stopping_criteria],
-                )
-        
+            )
+
         input_token_len = input_ids.shape[1]
         n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
         if n_diff_input_output > 0:
@@ -197,12 +200,12 @@ def eval_model_screenspot(args):
         if outputs.endswith(stop_str):
             outputs = outputs[:-len(stop_str)]
         outputs = outputs.strip()
-        
+
         # Plot Preds
         # pred_entities, pred_bboxes = find_bbox_template(outputs, img_w=img_w, img_h=img_h)
         # img = plot_refexp(img, pred_bboxes[0], pred_entities, mode="pred")
         # img.save('refexp_result/images/{}.png'.format(i))
-        
+
         ann["prompt"] = cur_prompt
         ann["output"] = outputs
         ann["operation"] = outputs
